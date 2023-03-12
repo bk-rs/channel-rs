fecd8b530d94c7ae5f6b36b67515786667b65b55
vkill <vkill.net@gmail.com>
2023-03-12 19:54:44 +0800

    Update channel-sender

--- channel-sender/src/impl_std.rs
+++ channel-sender/src/impl_std.rs
@@ -17,6 +17,12 @@ impl<T> Sender<T> for StdSyncSender<T> {
     }
 }
 
+impl<T> UnboundedSender<T> for StdSender<T> {
+    fn send(&self, t: T) -> Result<(), SendErrorWithoutFull<T>> {
+        StdSender::send(self, t).map_err(|err| SendErrorWithoutFull::Disconnected(err.0))
+    }
+}
+
 //
 impl<T> From<TrySendError<T>> for SendError<T> {
     fn from(err: TrySendError<T>) -> Self {
@@ -33,12 +39,27 @@ mod tests {
 
     #[test]
     fn test_with_channel() {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let sender: Box<dyn Sender<usize>> = Box::new(tx);
-        assert_eq!(sender.send(1), Ok(()));
-        assert_eq!(sender.send(2), Ok(()));
-        assert_eq!(rx.recv(), Ok(1));
-        assert_eq!(rx.recv(), Ok(2));
+        {
+            let (tx, rx) = std::sync::mpsc::channel();
+            let sender: Box<dyn Sender<usize>> = Box::new(tx);
+            assert_eq!(sender.send(1), Ok(()));
+            assert_eq!(sender.send(2), Ok(()));
+            assert_eq!(rx.recv(), Ok(1));
+            assert_eq!(rx.recv(), Ok(2));
+            drop(rx);
+            assert_eq!(sender.send(3), Err(SendError::Disconnected(3)));
+        }
+
+        {
+            let (tx, rx) = std::sync::mpsc::channel();
+            let sender: Box<dyn UnboundedSender<usize>> = Box::new(tx);
+            assert_eq!(sender.send(1), Ok(()));
+            assert_eq!(sender.send(2), Ok(()));
+            assert_eq!(rx.recv(), Ok(1));
+            assert_eq!(rx.recv(), Ok(2));
+            drop(rx);
+            assert_eq!(sender.send(3), Err(SendErrorWithoutFull::Disconnected(3)));
+        }
     }
 
     #[test]
@@ -48,5 +69,7 @@ mod tests {
         assert_eq!(sender.send(1), Ok(()));
         assert_eq!(sender.send(2), Err(SendError::Full(2)));
         assert_eq!(rx.recv(), Ok(1));
+        drop(rx);
+        assert_eq!(sender.send(3), Err(SendError::Disconnected(3)));
     }
 }
