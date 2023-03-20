81374d7006bc9c5d6f10381bd9f599a995d6ca06
vkill <vkill.net@gmail.com>
2023-03-20 22:16:01 +0800

    Update channel-receiver

--- channel-receiver/src/lib.rs
+++ channel-receiver/src/lib.rs
@@ -1,9 +1,9 @@
 //
 pub mod multi_consumer;
-pub use multi_consumer as mp;
+pub use multi_consumer as mc;
 
 pub mod single_consumer;
-pub use single_consumer as sp;
+pub use single_consumer as sc;
 
 pub mod one_shot;
 pub use one_shot as oneshot;
